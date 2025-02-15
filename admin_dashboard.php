d105227604067f7df7883616c052e6a2c5a5d54c
Djordje Stefanovic <djolestefanovic001@gmail.com>
2025-02-15 18:56:40 +0100

    new things

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -16,37 +16,78 @@ if(!isset($_SESSION['admin_id'])){
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Admin Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
     <meta name='viewport' content='width=device-width, initial-scale=1'>
 </head>
 <body>
 
-<div class ="container">
-    <div class="row mb-5">
-        <div class ="col-md-6">
-            <h2>Register Student</h2>
-            <form action="register_student.php" method="post" enctype="multipart/form-data">
-                First Name: <input class="form-control" type="text" name="first_name"><br>
-                Last Name: <input class="form-control" type="text" name="last_name"><br>
-                Email: <input class="form-control" type="email" name="email"><br>
-                Phone Number: <input class="form-control" type="text" name="phone_number"><br>
-                Session Plan:
-                <select class="form-control" name="session_plan_id">
-                    <option value="" disabled selected>Session Plan</option>
-                    <option value="1">12 sessions plan</option>
-                    <option value="2">30 sessions plan</option>                    
-                </select><br>
-                <input type="hidden" name="photo_path" id="photoPathInput">
+<?php if(isset($_SESSION['success_message'])) : ?>
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <?php echo $_SESSION['success_message'];
+    unset($_SESSION['seccess_message']);
+    ?>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+<?php endif; ?>
 
-                <div id="dropzone-upload" class="dropzone"></div>
+    <div class ="container">
+        <div class="row mb-5">
+            <div class ="col-md-6">
+                <h2>Register Student</h2>
+                <form action="register_student.php" method="post" enctype="multipart/form-data">
+                    First Name: <input class="form-control" type="text" name="first_name"><br>
+                    Last Name: <input class="form-control" type="text" name="last_name"><br>
+                    Email: <input class="form-control" type="email" name="email"><br>
+                    Phone Number: <input class="form-control" type="text" name="phone_number"><br>
 
-                <input class="btn btn-primary mt-3" type="submit" value="Register Member">
-                </form>  
+                    Session Plan:
+                    <select class="form-control" name="session_plan_id">
+                        <option value="" disabled selected>Session Plan</option>
+                        
+                        <?php
+                        $sql = "SELECT * FROM session_plans";
+                        $run = $conn->query($sql);
+                        $results = $run->fetch_all(MYSQLI_ASSOC);
+
+                        foreach($results as $result) {
+                            echo "<option value='" . $result['plan_id'] . "'>" . $result['name'] . "</option>";
+
+                        }
+                        ?>
+
+                    </select><br>
+                    <input type="hidden" name="photo_path" id="photoPathInput">
+
+                    <div id="dropzone-upload" class="dropzone"></div>
+
+                    <input class="btn btn-primary mt-3" type="submit" value="Register Member">
+                    </form>  
         </div>
     </div>
 </div>
 
+<script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
-
+<script>
+    Dropzone.option.dropzoneUpload = {
+        url: "upload_photo.php",
+        paramName: "photo",
+        maxFilesize: 20, //MB
+        acceptedFiles: "image/*",
+        init: function () {
+            this.on("success", function (file, response){
+                const jsonResponse = JSON.parse(response);
+                if (jsonResponse.success) {
+                    document.getElementById('photoPathInput').value = jsonResponse.photo_path;
+                } else {
+                    console.error(jsonResponse.error);
+                }
+            });
+        }
+    }
+</script>
 
 </body>
 </html>
--- register_student.php
+++ register_student.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'config.php';
+require_once 'fpdf/fpdf.php';
 
 if($_SERVER['REQUEST_METHOD'] == "POST") {
         $first_name = $_POST['first_name'];
@@ -11,7 +12,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
         $professor_id = 0;
         $photo_path = $_POST['photo_path'];
         $acces_card_pdf = "";
-}
+
 
         $sql = "INSERT INTO students (first_name, last_name, email, phone_number, photo_path, session_plan_id, professor_id, acces_card_pdf_path)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
@@ -19,3 +20,30 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
         $run = $conn->prepare($sql);
         $run->bind_param("sssssiis", $first_name, $last_name, $email, $phone_number, $photo_path, $session_plan_id, $professor_id, $acces_card_pdf);
         $run->execute();
+
+        $student_id = $conn->insert_id;
+
+        $pdf = new FPDF();
+        $pdf->AddPage();
+        $pdf->SetFont("Arial","B",16);
+
+        $pdf->Cell(40,10, 'Acces Card');
+        $pdf->Ln();
+        $pdf->Cell(40,10,'Member ID:' . $student_id);
+        $pdf->Ln();
+        $pdf->Cell(40,10,'Name: ' . $first_name . " " . $last_name);
+        $pdf->Ln();
+        $pdf->Cell(40,10,'Email: ' . $email);
+        $pdf->Ln();
+
+        $filename = 'access_cards/access_card_' . $student_id . '.pdf';
+        $pdf->Output('F',$filename);
+
+        $sql ="UPDATE students SET acces_card_pdf_path = '$filename' WHERE student_id = $student_id";
+        $conn->query($sql);
+
+        $_SESSION['success_message'] = 'Student successfully registered.';
+        header('location: admin_dashboard.php');
+        exit();
+
+}
